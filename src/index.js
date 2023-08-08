da471952a3547a08862fc015ddcd9796724d1d91
MuhammadTsabitAlAzam <73046372+MuhammadTsabitAlAzam@users.noreply.github.com>
2023-08-08 16:15:36 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-
+import express from 'express';
+import cors from 'cors'; // Import cors package
 import app from './app.js';
 import { PORT } from './config.js';
 
+const app = express();
 
+// Mengizinkan semua permintaan dari semua domain
+app.use(cors());
+
+// ... setup routes dan middleware lainnya ...
 
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
